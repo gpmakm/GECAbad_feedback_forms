393a3982a77f3c32ff725573148c8ee57067f1f0
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-05-19 19:10:44 +0530

    Added login page for faculty which will show name

--- src/app/backend/admin/route.js
+++ src/app/backend/admin/route.js
@@ -1,10 +1,32 @@
 import { Admin } from "mongodb";
 import { NextResponse } from "next/server";
-
+import connecttoDB from '../routes/connecttoDB'
 export async function POST(req) {
   try {
     const body = await req.json();
     const { username, password } = body;
+    let dbc=connecttoDB();
+    dbc.then((client) => {
+      const db = client.db();
+      const adminsCollection = db.collection("admins");
+      adminsCollection.findOne({ username }).then((admin) => {
+        if (admin && admin.password === password) {
+          const response = NextResponse.json({
+            admin: username,
+            success: true,
+            message: "Login successful"
+          });
+          response.cookies.set(process.env.AUTH_TOKEN, "true", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 60 * 60 * 3,
+            path: "/"
+          });
+
+          return response;
+        }
+      });
+    });
 
     if (password === process.env.USER_PASSWORD) {
 
